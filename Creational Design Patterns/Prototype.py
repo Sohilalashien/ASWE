0f8ddf79cd2e8a0effe92defd82a8ab9f13e132f
Menna-Medhat <162800973+Menna-Medhat@users.noreply.github.com>
2024-05-24 22:29:34 +0300

    Prototype.py

--- Creational Design Patterns/Prototype.py
+++ Creational Design Patterns/Prototype.py
@@ -1,52 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
+import copy
 
-# In[6]:
-
-
-"Prototype Concept Sample Code"
-
-from abc import ABCMeta, abstractmethod
-
-class IProtoType(metaclass=ABCMeta):
-    "interface with clone method"
-    @staticmethod
-    @abstractmethod
-    def clone():
-        """The clone, deep or shallow.
-        It is up to you how you want to implement
-        the details in your concrete class"""
-
-class MyClass(IProtoType):
-    "A Concrete Class"
-
-    def __init__(self, field):
-        self.field = field  # any value of any type
-
+class Shape:
+    "Interface with clone method"
     def clone(self):
-        " This clone method uses a shallow copy technique "
-        return type(self)(
-            self.field  # a shallow copy is returned
-        )
+        # Using deepcopy to ensure a deep copy of the object
+        return copy.deepcopy(self)
+
+class Circle(Shape):
+    "A Concrete Class"
+    def __init__(self, radius):
+        self.radius = radius
 
     def __str__(self):
-        return f"{id(self)}\tfield={self.field}\ttype={type(self.field)}"
+        return f"Circle with radius {self.radius}"
 
 # The Client
-OBJECT1 = MyClass([1, 2, 3, 4])  # Create the object containing a list
-print(f"OBJECT1 {OBJECT1}")
+circle = Circle(5)
+clone_circle = circle.clone()
 
-OBJECT2 = OBJECT1.clone()  # Clone
-
-OBJECT2.field[1] = 101
-
-# Comparing OBJECT1 and OBJECT2
-print(f"OBJECT2 {OBJECT2}")
-print(f"OBJECT1 {OBJECT1}")
-
-
-# In[ ]:
-
-
-
-
+# Comparing the original object and the cloned object
+print(circle)      
+print(clone_circle) 
